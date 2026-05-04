0b7edacf770efbe59d1ff2afaa0e96511a8c2f5f
Bruno Guzzo <brunoguzzo18@gmail.com>
2026-05-04 22:49:27 +0100

    Add: ideal portfolio on IBKR

--- compute_idxs.sh
+++ compute_idxs.sh
@@ -15,3 +15,10 @@ uv run mkt-anlys-cli.py \
 --outdir "./results/sp20_eq_weight" \
 --period "3mo, 6mo, 1y, 3y, 5y, 10y" \
 --idx_name "sp20_eq_weight"
+
+uv run mkt-anlys-cli.py \
+--weights "$(cat ./configs/ibkr_portfolio.json)" \
+--benchmark 'VOO' \
+--outdir "./results/ibkr_portfolio_50_50_voo" \
+--period "3mo, 6mo, 1y, 3y, 5y, 10y" \
+--idx_name "ibkr_portfolio"
